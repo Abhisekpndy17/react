3813f5ac962f99c0146ce55094b6de7ae142d548
Abhishek Pandey <abhishekbca15.iimtgn@gmail.com>
2021-03-06 19:19:36 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='navbar'>
+            <h1>The Blog</h1>
+            <div className='links'>
+                <a href='/'>home</a>
+                <a href='#'>Info</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Home.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useState } from 'react';
+
+
+export default function Home() {
+    const [blogs, setBlogs] = useState([
+        {
+            title:'This is first title of blog one',
+            body:'This is the body elements of the vlog post one',
+            author:'Deepak Pandit',
+            id:1,
+        },
+        {
+            title:'This is first title of blog two',
+            body:'This is the body elements of the vlog post two',
+            author:'Veer ',
+            id:2,
+        },
+        {
+            title:'This is first title of blog three',
+            body:'This is the body elements of the vlog post three',
+            author:'Brijendra',
+            id:3,
+        },
+        {
+            title:'This is first title of blog four',
+            body:'This is the body elements of the vlog post four',
+            author:'Abhishek',
+            id:4,
+        }
+    ])
+    return (
+        <div>
+            <h2>This is Home Page</h2>
+            <div className='grid'>
+                {blogs.map((blog) =>(
+                    <div className='card' key={blog.id}>
+                        <h2>{blog.title}</h2>
+                        <p>{blog.body}</p>
+                        
+                        <p className='padding'>Author : {blog.author}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
